c7971cebb98a85c19307a4182bb90b8bf83af713
Michał Ziembiński <ziembinski.michal@gmail.com>
2021-10-28 21:42:33 +0200

    Dalsza część szkolenia

--- flask/market/routes.py
+++ flask/market/routes.py
@@ -1,19 +1,23 @@
 from market import app
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm, LoginForm
+from market.forms import RegisterForm, LoginForm, PurchaseItemForm
 from market import db
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template('home.html')
 
-@app.route('/market')
+@app.route('/market', methods=['GET', 'POST'])
+@login_required
 def market_page():
+    purchase_form = PurchaseItemForm()
+    if purchase_form.validate_on_submit():
+        print(purchase_form['submit'])
     items = Item.query.all()
-    return render_template('market.html', items=items)
+    return render_template('market.html', items=items, purchase_form=purchase_form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
@@ -24,6 +28,10 @@ def register_page():
                               password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
+
+        login_user(user_to_create)
+        flash(f'Account created succesfully! You are log i as {user_to_create.username}', category = 'success')
+
         return redirect(url_for('market_page'))
     if form.errors != {}: #If there are not errors from the validations
         for err_msg in form.errors.values():
@@ -46,3 +54,10 @@ def login_page():
             flash('Username and password are not match! Please try again', category='danger')
 
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logout", category='info')
+    return redirect(url_for("home_page"))
